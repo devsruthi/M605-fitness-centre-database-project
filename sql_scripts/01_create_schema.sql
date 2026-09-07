9efce8331b291b27e7c50add2a6dab11bea4c483
Sruthi P S <72307859+devsruthi@users.noreply.github.com>
2026-09-07 06:27:38 +0200

    Delete sql_scripts directory

--- sql_scripts/01_create_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA fitness_wellness_center_system;
-
-USE fitness_wellness_center_system;
-
-set sql_safe_updates = 0;
-
-Show variables like 'local_infile';
-Set global local_infile = 1;
